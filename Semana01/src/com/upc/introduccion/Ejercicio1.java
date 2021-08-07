0ac946d506dad245e3dd48252b9a138a16a99cd2
Carlos Alberto <pciscflo@upc.edu.pe>
2021-08-07 14:41:30 -0500

    first commit

---
+++ Semana01/src/com/upc/introduccion/Ejercicio1.java
@@ -0,0 +1,10 @@
+package com.upc.introduccion;
+
+public class Ejercicio1 {
+
+     public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Bienvenido"); //sou
+        System.out.println("Gracias.");
+    }
+}
