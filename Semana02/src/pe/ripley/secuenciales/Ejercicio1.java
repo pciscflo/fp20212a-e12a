ed726068d89ed23b32b9acd3dd6f136fe7ac0b04
Carlos Alberto <pciscflo@upc.edu.pe>
2021-08-14 08:38:40 -0500

    01.00

---
+++ Semana02/src/pe/ripley/secuenciales/Ejercicio1.java
@@ -0,0 +1,18 @@
+package pe.ripley.secuenciales;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String marca;
+        int edad;
+        double peso;
+        int contador = 0;
+        int nota1=20, nota2=15;
+        double promedio;
+
+        promedio = (nota1 + nota2) /2.0;
+
+        System.out.println(promedio);
+
+
+    }
+}
