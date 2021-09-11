e774e75f17787d9606df96e8894e9edde5634d6e
Carlos Alberto <pciscflo@upc.edu.pe>
2021-09-11 07:20:07 -0500

    Semana6-01

---
+++ Semana06/src/com/upc/arreglos/Ejercicio1.java
@@ -0,0 +1,20 @@
+package com.upc.arreglos;
+
+public class Ejercicio1 {
+    /*
+   Una empresa vendedora de fotocopiadoras necesita calcular cual ha sido el volumen de ventas de cada uno de sus
+   agentes comerciales durante el presente mes y en base a eso determinar distintos indicadores, así como montos
+   a pagar en comisión de ventas. Se le solicita:
+   a) Obtener el total de unidades vendidas durante el presente mes. Para eso se tiene como dato de entrada un
+    arreglo con la cantidad de unidades vendidas por cada agente comercial.
+   b) Si se sabe que la cuota de venta de cada vendedor es 5 unidades, determinar cuál es el porcentaje de vendedores
+    que superaron la cuota de venta. Para eso se tiene como dato de entrada un arreglo con la cantidad de unidades
+     vendidas por cada agente comercial.
+   c) Se desea saber quién(es) fueron los agentes de ventas que superaron la cuota de venta.
+   d) Si se sabe que por cada unidad vendida se tiene que pagar una comisión de ventas de 300 soles, determinar
+    cuánto es lo que se tiene que pagar como comisión este mes.
+   */
+    public static void main(String[] args) {
+
+    }
+}
