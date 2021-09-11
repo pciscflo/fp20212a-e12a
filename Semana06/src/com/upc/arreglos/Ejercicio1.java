de1399042510753e48f47ca6c698efac8ad0af8c
Carlos Alberto <pciscflo@upc.edu.pe>
2021-09-11 08:15:27 -0500

    Semana6-02

--- Semana06/src/com/upc/arreglos/Ejercicio1.java
+++ Semana06/src/com/upc/arreglos/Ejercicio1.java
@@ -15,6 +15,56 @@ public class Ejercicio1 {
     cuánto es lo que se tiene que pagar como comisión este mes.
    */
     public static void main(String[] args) {
+        String[] vendedores = {"Carlos", "Freddy", "Marcos", "Susan", "Pedro", "Luis", "Karen"};
+        int[] cantidadVentas = {10, 6, 14, 7 ,4, 5, 3};//i=0, 1....
+        System.out.println("Total dee unidades vendidas:"+ sumarVentas(cantidadVentas));
+        System.out.println("Cantidad de vendedores que superaron la cuota:" +
+                cantidadVendedoresSuperaronCuota(cantidadVentas));
+        System.out.println("**Vendedores que Superaron Cuota***");
+        String[] vendedoreCuota;
+        vendedoreCuota = obtenerVendedoresSuperaronCuota(cantidadVentas,vendedores);
+        for(int i=0; i<vendedoreCuota.length;i++){
+            System.out.println(vendedoreCuota[i]);
+        }
+        System.out.println("Ventas a pagar:"+sumarVentasAPagar(cantidadVentas));
 
+    }
+    public static int sumarVentas(int[] cantidadVentas){
+        int suma=0;
+        for (int i=0; i < cantidadVentas.length;i++){//1,2,..6,7
+            suma+=cantidadVentas[i];//10+6+14....+3
+        }
+        return suma;
+    }
+    public static int cantidadVendedoresSuperaronCuota(int[] cantidadVentas){
+        int contador=0;
+        for(int i=0; i< cantidadVentas.length;i++){
+            if (cantidadVentas[i]>=5){
+                contador++;
+            }
+        }
+        return contador;
+    }
+    public static String[] obtenerVendedoresSuperaronCuota(int[] cantidadVentas, String[] vendedores){
+        String[] vendedoresSuperaron = new String[cantidadVentas.length]; // no se puede poner variable, en array estatico
+        //{"Carlos", "Freddy", "Marcos"", null, null, null, null} //j =
+        int j = 0;
+        for (int i=0; i< cantidadVentas[i];i++){//0,1,2
+            if(cantidadVentas[i]>=5){
+                vendedoresSuperaron[j] = vendedores[i];
+                j++;//1,2
+            }
+        }
+        //opcional, obtener el arreglo sin espacios vacíos si es que quedan
+        String [] arrF = new String[j];
+        System.arraycopy(vendedoresSuperaron, 0, arrF, 0, arrF.length);
+        return arrF;///lo clásico es que se ponga return temporal;
+    }
+    public static int sumarVentasAPagar(int[] cantidadVentas){
+        int suma=0;
+        for (int i=0; i < cantidadVentas.length;i++){//1,2,..6,7
+            suma+=cantidadVentas[i]*300;
+        }
+        return suma;
     }
 }
