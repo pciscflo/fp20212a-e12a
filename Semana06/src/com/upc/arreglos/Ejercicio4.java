c040a65b867721b9e97ffe5c3287b10704e1de9b
Carlos Alberto <pciscflo@upc.edu.pe>
2021-09-11 09:53:17 -0500

    Semana6-04

---
+++ Semana06/src/com/upc/arreglos/Ejercicio4.java
@@ -0,0 +1,28 @@
+package com.upc.arreglos;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        String[] codigos = {"AR000101", "PE125610", "CH452315", "BR521317", "BR123620", "PE752124",
+        "AR124511", "PE562405", "PE562405", "BR452102", "AR455210"};
+        System.out.println("Cantidad de Productos:" + obtenerCantidadProductos("PE",codigos));
+        System.out.println("Maximo Correlativo dado pais:" + obtenerMayorCorrelativo("BR", codigos));
+    }
+    public static int obtenerCantidadProductos(String nacionalidad,String[] codigos){
+        int contador=0;
+        for(int i=0; i<codigos.length;i++){
+            if(codigos[i].substring(0,2).equals(nacionalidad)){
+                contador++;
+            }
+        }
+        return contador;
+    }
+    public static int obtenerMayorCorrelativo(String nacionalidad, String[] codigos){
+        int maximo = 0;
+        for(int i=0; i<codigos.length;i++){
+            if(Integer.parseInt(codigos[i].substring(2,6)) > maximo && codigos[i].substring(0,2).equals(nacionalidad)){
+                maximo = Integer.parseInt(codigos[i].substring(2,6));
+            }
+        }
+        return maximo;
+    }
+}
